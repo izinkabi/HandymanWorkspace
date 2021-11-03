b0da358d886cfb70ac372369c309759bbda97964
zingisanimajama <zingisanimajama@gmail.com>
2021-11-03 20:10:24 +0200

    Bug fix in Request table

--- Handyman/HandymanDB/Tables/Request.sql
+++ Handyman/HandymanDB/Tables/Request.sql
@@ -3,7 +3,6 @@
 	[RequestId] INT NOT NULL PRIMARY KEY, 
     [consumerId] INT NOT NULL, 
     [ProvidersServiceId] INT NOT NULL, 
-    [ServiceProviderId] INT NOT NULL,  
     CONSTRAINT [FK_Request_Consumer] FOREIGN KEY ([ConsumerId]) REFERENCES [Consumer]([ConsumerId]),
     CONSTRAINT [FK_Request_ProvidersServiceId] FOREIGN KEY ([ProvidersServiceId]) REFERENCES [ProvidersService]([Id])
 
