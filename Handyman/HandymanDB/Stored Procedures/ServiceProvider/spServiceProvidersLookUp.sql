d7d6c1c07f12e11b36a1a49b26addd274ec477e8
Nkosinathi Nkosi <nkosinathi.vodacom@gmail.com>
2021-11-18 17:11:55 +0200

    #added a Stored Procesure Folder for Service Provider

---
+++ Handyman/HandymanDB/Stored Procedures/ServiceProvider/spServiceProvidersLookUp.sql
@@ -0,0 +1,15 @@
+﻿
+CREATE PROCEDURE [dbo].[spServiceProvidersLookUp]
+	
+
+	@ProfileId int,
+	@CompanyName VARCHAR (50)='',
+	@ProviderType VARCHAR(50) =''
+	
+	
+
+AS
+Begin
+	INSERT INTO [ServiceProvider] (CompanyName,ProfileId,ProviderType)
+	VALUES (@ProfileId,@CompanyName,@ProviderType)
+End
