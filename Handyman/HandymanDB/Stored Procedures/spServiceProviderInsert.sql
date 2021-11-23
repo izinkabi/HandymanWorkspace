06485a53087186cb5faa22d9a8281aed9ceccd0d
Nkosinathi Nkosi <nkosinathi.vodacom@gmail.com>
2021-11-23 19:47:22 +0200

    #Restructured the Service Provider display mode.

--- Handyman/HandymanDB/Stored Procedures/spServiceProviderInsert.sql
+++ Handyman/HandymanDB/Stored Procedures/spServiceProviderInsert.sql
@@ -1,11 +1,9 @@
 ﻿/*Inserting a ServiceProvider SP*/
 CREATE PROCEDURE [dbo].[spServiceProviderInsert]
 	
-	@Id int,
 	@ProfileId int,
 	@CompanyName VARCHAR (50)='',
 	@ProviderType VARCHAR(50) =''
-	
 
 AS
 Begin
