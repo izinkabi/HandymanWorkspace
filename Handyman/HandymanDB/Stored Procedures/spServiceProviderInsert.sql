c5cca8ef8594ad3f3d723d8eec9fa9bdc491fb66
Nkosinathi Nkosi <nkosinathi.vodacom@gmail.com>
2021-12-02 22:09:39 +0200

    #Edited Service Provider to make it function and display Service Provider Info

--- Handyman/HandymanDB/Stored Procedures/spServiceProviderInsert.sql
+++ Handyman/HandymanDB/Stored Procedures/spServiceProviderInsert.sql
@@ -1,12 +1,11 @@
 ﻿/*Inserting a ServiceProvider SP*/
 CREATE PROCEDURE [dbo].[spServiceProviderInsert]
-	@Id int,
 	@ProfileId int,
 	@CompanyName VARCHAR (50)='',
 	@ProviderType VARCHAR(50) =''
 
 AS
 Begin
-	INSERT INTO [ServiceProvider] (CompanyName,ProviderType)
-	VALUES (@CompanyName,@ProviderType)
+	INSERT INTO [ServiceProvider] (ProfileId,CompanyName,ProviderType)
+	VALUES (@ProfileId,@CompanyName,@ProviderType)
 End
