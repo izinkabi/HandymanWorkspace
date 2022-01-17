204847d2a2a4895553d9453bb80941e6d3a1de8b
Nkosinathi <nkosinathi.vodacom@gmail.com>
2022-01-17 19:21:37 +0200

    #Added Email Pages --Not complete--

---
+++ Handyman/Handyman_UI/Controllers/Email/EmailController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Handyman_UI.Controllers.Email
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Inbox()
+        {
+            return View();
+        }
+        public ActionResult ComposeEmail()
+        {
+            return View();
+        }
+        public ActionResult Read()
+        {
+            return View();
+        }
+    }
+}
