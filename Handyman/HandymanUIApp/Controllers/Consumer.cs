ad3b556f7fdd837d34bf625f912a1c915779046c
zingisanimajama <zingisanimajama@gmail.com>
2022-07-16 02:50:50 +0200

    Decorated details and services

--- Handyman/HandymanUIApp/Controllers/Consumer.cs
+++ Handyman/HandymanUIApp/Controllers/Consumer.cs
@@ -7,61 +7,18 @@ namespace HandymanUIApp.Controllers
     public class Consumer : Controller
     {
       
-        protected List<ServiceModel>? AllServices;
-        private List<HandymanUILibrary.Models.ServiceModel> servicesData;
-        private IServiceEndPoint? _serviceEndpoint;
-        string errorString;
-        private IAPIHelper _apiClient;
+        
 
-        public Consumer(IAPIHelper aPIHelper, IServiceEndPoint seviceEndPoint)
+        public Consumer(IAPIHelper aPIHelper)
         {
-            _apiClient = aPIHelper;
-            _serviceEndpoint = seviceEndPoint;
+            //_apiClient = aPIHelper;
+           // _serviceEndpoint = seviceEndPoint;
 
         }
-        internal async Task<IEnumerable<ServiceModel>> LoadServices()
-        {
-            AllServices = new List<ServiceModel>();
-            servicesData =  new List<HandymanUILibrary.Models.ServiceModel>();
-            try
-            {
-                servicesData = await _serviceEndpoint?.GetServices();
-            }catch(Exception ex)
-            {
-                errorString = ex.Message;
-            }
-            
-            foreach(var service in servicesData)
-            {
-                //populating a service to UI model
-                var s = new ServiceModel();
-                s.Name = service.Name;
-                s.Decription = service.Description;
-                s.ImageUrl = service.ImageUrl;
-                s.Id = service.Id;
-                s.Category = new CategoryModel();
-                s.Category.Name = service.Name;
-                s.Category.Description = service.Description;
-                AllServices.Add(s);
-            }
-            return AllServices;
-        }
+       
+       
         // GET: Consumer
-        public async Task<ActionResult> Index()
-        {
-             
-            try
-            {
-                var svs = await LoadServices();
-                    return View(svs);
-            }
-            catch (Exception ex)
-            {
-                errorString = ex.Message;
-                return Redirect("Error");
-            }
-            
-        }
+        
        
         // GET: Consumer/Details/5
         public ActionResult Details(int id)
