c43aac9f1cf48fc0ac55a64a98e7f218e160a743
zingisanimajama <zingisanimajama@gmail.com>
2022-09-12 21:42:25 +0200

    DB: Added the todo table only

---
+++ Handyman/HandymanDB/Tables/ToDo.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [Service].[ToDo]
+(
+	[Id] INT NOT NULL, 
+    [ItemName] VARCHAR(150) NOT NULL, 
+    [Description] VARCHAR(250) NOT NULL, 
+    [OrderId] INT NOT NULL, 
+    [Type] INT NOT NULL, 
+    [StartDate] DATETIME2 NULL, 
+    [FinishDate] DATETIME2 NULL, 
+    [Status] INT NOT NULL,
+    CONSTRAINT [PK_ServiceTodo] PRIMARY KEY ([Id] ASC),
+    CONSTRAINT [FK_Service.Todo_Customer.Order] FOREIGN KEY ([OrderId]) REFERENCES [Customer].[Order] ([Id])
+)
