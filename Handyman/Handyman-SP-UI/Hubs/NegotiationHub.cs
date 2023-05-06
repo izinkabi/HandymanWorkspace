889eff89b5c0fd145cf63bbf0b97f02dba03a600
zingisanimajama <zingisanimajama@gmail.com>
2023-05-06 23:54:05 +0200

    Add SignalR update on Hub parameters

--- Handyman/Handyman-SP-UI/Hubs/NegotiationHub.cs
+++ Handyman/Handyman-SP-UI/Hubs/NegotiationHub.cs
@@ -24,4 +24,18 @@ public class NegotiationHub : Hub
         return Clients.All.SendAsync("ReceiveNegoUpdate", user, dueDate, total);
     }
 
+    public async Task AddToGroup(string groupName)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+        await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+    }
+
+    public async Task RemoveFromGroup(string groupName)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+        await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+    }
+
 }
