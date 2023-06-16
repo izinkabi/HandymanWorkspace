f9443aad41470f6a099a7b48e999a2612632106c
zingisanimajama <zingisanimajama@gmail.com>
2023-06-16 23:44:11 +0200

    UI-SP: Add Shape Loader css and htm

--- Handyman/Handyman_DataLibrary/Helpers/EmailSender.cs
+++ Handyman/Handyman_DataLibrary/Helpers/EmailSender.cs
@@ -11,14 +11,14 @@ namespace Handyman_DataLibrary.Helpers
         public void SendEmail(string body)
         {
             var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse("araceli.powlowski@ethereal.email"));
-            email.To.Add(MailboxAddress.Parse("araceli.powlowski@ethereal.email"));
+            email.From.Add(MailboxAddress.Parse("major81@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse("major81@ethereal.email"));
             email.Subject = "Confirm Email";
             email.Body = new TextPart(TextFormat.Html) { Text = body };
 
             using var smtp = new SmtpClient();
             smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
-            smtp.Authenticate("araceli.powlowski@ethereal.email", "6dwzhKNuGPZwgxvmzr");
+            smtp.Authenticate("major81@ethereal.email", "xUxbtMqCzcVf7QsWx9");
             smtp.Send(email);
             smtp.Disconnect(true);
 
