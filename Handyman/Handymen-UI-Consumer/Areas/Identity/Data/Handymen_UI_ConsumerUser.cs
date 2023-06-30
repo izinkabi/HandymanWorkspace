be8f61ff15c7aea03c5daabd3aa332ee741cc9f9
zingisanimajama <zingisanimajama@gmail.com>
2023-06-30 22:23:38 +0200

    Delete Razor Pages UI

--- Handyman/Handymen-UI-Consumer/Areas/Identity/Data/Handymen_UI_ConsumerUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Handymen_UI_Consumer.Areas.Identity.Data;
-
-// Add profile data for application users by adding properties to the Handymen_UI_ConsumerUser class
-public class Handymen_UI_ConsumerUser : IdentityUser
-{
-}
-
