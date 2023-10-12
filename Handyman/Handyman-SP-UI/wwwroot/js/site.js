2b9d4975675d3f4b4d1fb459e939723e9a3196fe
Nkosinathi Nkosi <nkosi_na04@outlook.com>
2023-10-12 22:22:41 +0200

    eddited the dashboard

--- Handyman/Handyman-SP-UI/wwwroot/js/site.js
+++ Handyman/Handyman-SP-UI/wwwroot/js/site.js
@@ -1,1 +1,29 @@
 ﻿// Write your Javascript code.
+//TODO - Animations
+//TODO -    FORM VALIDATION
+//TODO - Chart display
+
+//* This how we pass data and chart set up to 
+const ctx = document.getElementById('bar-chart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
+      datasets: [{
+        label: 'Workshop',
+        data: [1, 9, 3, 5, 2, 3, 4, 7, 3, 5, 2, 3],
+        borderWidth: 1,
+        backgroundColor: '#849BA0',
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  console.log(Chart);
